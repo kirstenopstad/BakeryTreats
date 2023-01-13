b7d01d9b55e5ba4e7ada063c2bb02130936c0166
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-01-13 11:25:57 -0800

    add Details & AddFlavor routes

--- Bakery/Controllers/TreatsController.cs
+++ Bakery/Controllers/TreatsController.cs
@@ -40,7 +40,32 @@ namespace Bakery.Controllers
     }
 
     // Details
+    public ActionResult Details(int id)
+    {
+      // Pass in list of flavors
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Type");
+      Treat thisTreat = _db.Treats
+                           .Include(treat => treat.FlavorTreats)
+                           .ThenInclude(flavTreat => flavTreat.Flavor)
+                           .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
     
+    // AddFlavor
+    public ActionResult AddFlavor(Treat treat, int flavorId)
+    {
+      # nullable enable
+      FlavorTreat? assoc = _db.FlavorTreats
+                              .FirstOrDefault(assoc => (assoc.TreatId == treat.TreatId && assoc.FlavorId == flavorId));
+      # nullable disable
+      if (assoc == null && flavorId != 0)
+      {
+        _db.FlavorTreats.Add(new FlavorTreat { TreatId = treat.TreatId, FlavorId = flavorId});
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", "Treats", new { id = treat.TreatId});
+    }
+
     // Edit  POST
     
     // Delete POST
